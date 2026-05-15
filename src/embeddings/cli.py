0810e52b138daf2a21d522051077ccda22fb31c7
jwardbond <jessewb.c@gmail.com>
2026-05-15 17:39:20 -0400

    Removed my attempt at hiding pytorch errors

--- src/embeddings/cli.py
+++ src/embeddings/cli.py
@@ -1,14 +1,11 @@
 import logging
-import warnings
+
+logging.getLogger("torch.utils.flop_counter").setLevel(logging.ERROR)
 
 from lightning.pytorch.cli import LightningCLI
 
 import embeddings.dataloaders
 import embeddings.models
-
-# Silence the Triton warning from torch.utils.flop_counter
-warnings.filterwarnings("ignore", category=UserWarning, module="torch.utils.flop_counter")
-logging.getLogger("torch.utils.flop_counter").setLevel(logging.ERROR)
 
 
 def run_cli():
